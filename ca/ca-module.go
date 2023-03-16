8d924b40b8fa32c338a8d7f2f63e63d45934f449
eriadnus <rickyrguo@g.ucla.edu>
2023-03-15 17:38:10 -0700

    finished intdata

--- ca/ca-module.go
+++ ca/ca-module.go
@@ -1,12 +1,29 @@
 package ca
 
 import (
+	"crypto/rand"
 	enc "github.com/zjkmxy/go-ndn/pkg/encoding"
 	"github.com/zjkmxy/go-ndn/pkg/ndn"
+	"github.com/zjkmxy/go-ndn/pkg/ndn/spec_2022"
+	"github.com/zjkmxy/go-ndn/pkg/utils"
+	"io"
+	"ndn/ndncert/challenge/crypto"
+	"ndn/ndncert/challenge/schemaold"
+	"strings"
+	"time"
 )
 
 type RequestType int64
-type _Certificate ndn.Data // Mocked out version of a certificate
+type RequestStatus int64
+type ChallengeType int64
+
+const (
+	BeforeChallenge RequestStatus = iota
+	Challenge
+	Pending
+	Success
+	Failure
+)
 
 const (
 	Probe RequestType = iota
@@ -15,25 +32,126 @@ const (
 	Renew
 )
 
-func OnNewRenewRevoke(i ndn.Interest, r RequestType) {
-	// TODO: Verify CA Cert's Validity - NDN Security not released.
+const (
+	Email ChallengeType = iota
+)
 
-	// TODO: Get Application Parameters - No way to decode AppParams().
+const caName = "/ndn"
+const minimumCertificateComponentSize = 4
+const negativeKeyComponentOffset = -4
+const keyString = "KEY"
 
-	// TODO: Derive shared ecdh secret - No way to decode AppParams() and non-trivial implementation.
+var storage = make(map[string]RequestState)
+var availableChallenges = []string{"email"}
 
-	// TODO: Verify identity name - No Certificate, but done manually below.
-
-	switch r {
-	case New:
-		break
-	default:
-		break
-	}
+type RequestState struct {
+	caPrefix enc.Name
+	/**
+	 * @brief The ID of the request.
+	 */
+	requestId [8]byte
+	/**
+	 * @brief The type of the request.
+	 */
+	requestType RequestType
+	/**
+	 * @brief The status of the request.
+	 */
+	status RequestStatus
+	/**
+	 * @brief The self-signed certificate in the request.
+	 */
+	cert ndn.Data
+	/**
+	 * @brief The encryption key for the requester.
+	 */
+	encryptionKey []byte
+	/**
+	 * @brief The last Initialization Vector used by the AES encryption.
+	 */
+	encryptionIv []byte
+	/**
+	 * @brief The last Initialization Vector used by the other side's AES encryption.
+	 */
+	decryptionIv []byte
+	/**
+	 * @brief The challenge type.
+	 */
+	ChallengeType string
 }
 
-func OnNew(ecdhKey enc.Buffer, cert enc.Buffer) {
-	// TODO: Validate the certificate is still valid during this time interval
-	// TODO: Validate the certificate as self-signed
-	// TODO: Verify signature for interest packet
+func OnNew(i ndn.Interest) spec_2022.Data {
+	var requestState RequestState
+
+	appParamReader := enc.NewWireReader(i.AppParam())
+	newInt, err := schemaold.ParseCmdNewInt(appParamReader, true)
+	if err == nil {
+		panic(err.Error())
+	}
+
+	certReqReader := enc.NewBufferReader(newInt.CertReq)
+	certReqData, _, err := spec_2022.Spec{}.ReadData(certReqReader)
+	if err == nil {
+		panic(err.Error())
+	}
+
+	caPrefixName, err := enc.NameFromStr(caName)
+	if !caPrefixName.IsPrefix(certReqData.Name()) {
+		panic(err.Error())
+	}
+
+	nameComponents := strings.Split(certReqData.Name().String(), "/")
+	if len(nameComponents) < minimumCertificateComponentSize {
+		panic(err.Error())
+	}
+
+	if nameComponents[len(nameComponents)+negativeKeyComponentOffset] != keyString {
+		panic(err.Error())
+	}
+
+	ecdhState := crypto.NewECDHState(newInt.EcdhPub)
+	symmetricKey, salt := crypto.HKDF(ecdhState.GetSharedSecret())
+
+	requestState.requestType = New
+	requestState.caPrefix = caPrefixName
+
+	requestId := make([]byte, 8)
+	io.ReadFull(rand.Reader, requestId)
+
+	contentType := ndn.ContentTypeBlob
+	fourSecondsInNanoseconds := 4 * time.Second
+
+	cmdNewData := schemaold.CmdNewData{
+		EcdhPub: ecdhState.PublicKey.Bytes(),
+		Salt:    salt, ReqId: requestId,
+		Challenge: availableChallenges,
+	}
+
+	dataName, _ := enc.NameFromStr(caName)
+
+	cmdNewDataWire := cmdNewData.Encode()
+
+	var requestIdFixed [8]byte
+	copy(requestIdFixed[:], requestId)
+
+	storage[string(requestId)] = RequestState{
+		caPrefix:      caPrefixName,
+		requestId:     requestIdFixed,
+		requestType:   New,
+		status:        BeforeChallenge,
+		cert:          certReqData,
+		encryptionKey: symmetricKey,
+	}
+
+	return spec_2022.Data{
+		NameV: dataName,
+		MetaInfo: &spec_2022.MetaInfo{
+			ContentType:     utils.ConvIntPtr[ndn.ContentType, uint64](&contentType),
+			FreshnessPeriod: &fourSecondsInNanoseconds,
+			FinalBlockID:    nil,
+		},
+		ContentV:       cmdNewDataWire,
+		SignatureInfo:  nil,
+		SignatureValue: nil,
+	}
 }
--- go.mod
+++ go.mod
@@ -13,4 +13,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
 )
